0e008a490b455330655583747d3327bd1adb00dc
Alessio Rugi <alessiorugi95@gmail.com>
2024-01-07 18:32:01 +0100

    Fine

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,12 +1,18 @@
 <script>
+import ProductCard from './ProductCard.vue'
+
 export default {
-    name: "AppMain"
+    name: "AppMain",
+    components: {
+        ProductCard,
+    },
 }
 </script>
 
 <template>
     <section class="container">
-        <h1>Content goes here</h1>
+        <h1>CURRENT SERIES</h1>
+        <ProductCard />
     </section>
 </template>
 
@@ -16,11 +22,15 @@ export default {
 .container {
     min-height: 150px;
     display: flex;
-    align-items: center;
-}
-h1 {
-    font-size: 30px;
-    color: $white;
+    flex-wrap: wrap;
+    justify-content: center;
 }
 
+h1 {
+    color: $white;
+    margin: 10px 0;
+    padding: 10px 40px;
+    background-color: $blue;
+    font-size: 25px;
+}
 </style>
